1c364530c98a4bb94030c33a674502231171f89a
KaiTang26 <16kt16@queensu.ca>
2019-04-25 14:27:08 -0400

    popular done

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,20 +2,35 @@ var React = require('react');
 
 class Popular extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state={
+            selectedLang: "All"
+        };
+        //this.handleLangUpdate = this.handleLangUpdate.bind(this);
+    }
+
+    handleLangUpdate(lang){
+        this.setState(function(){
+            return {
+                selectedLang: lang}
+        })
+    }
+    
     render(){
-        var language = ["All", "Jave", "Python", "Javascript"];
+        var language = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
         return(
-            <div>
-                <ul>
+                <ul className="language">
                     {language.map(function(lang, index){
                         return (
-                            <li key={index}>
+                            <li key={index} 
+                                onClick={this.handleLangUpdate.bind(this,lang)}
+                                style={lang === this.state.selectedLang ? {color: "#d0021b"}: null}>
                                 {lang}
                             </li>
                         )
-                    })}
+                    }, this)}
                 </ul>  
-            </div>
         )
     }
 }
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 var App = require('./components/App');
+require("./index.css");
 
 ReactDom.render(<App />, document.getElementById('app'));
